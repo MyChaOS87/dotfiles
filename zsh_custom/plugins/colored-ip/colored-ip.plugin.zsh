0572d196bba0d7fb755126b4c1ca1852413f5a25
Michael Kasch <kasch@smartcast.de>
2015-01-02 11:28:29 +0100

    coloring not only link/ether but link/[a-z]* in ip output

--- zsh_custom/plugins/colored-ip/colored-ip.plugin.zsh
+++ zsh_custom/plugins/colored-ip/colored-ip.plugin.zsh
@@ -11,7 +11,7 @@ function colored_ip () {
     $IP_CMD $@ | sed \
     -e "s/inet [^ ]\+ /${IPV4_COLOR}&${reset_color}/g"\
     -e "s/inet6 [^ ]\+ /${IPV6_COLOR}&${reset_color}/g"\
-    -e "s/link\/ether [^ ]\+ /${LINK_COLOR}&${reset_color}/g"\
+    -e "s/link\/[a-z]* [^ ]\+ /${LINK_COLOR}&${reset_color}/g"\
     -e "s/^default via .*$/${DEFAULT_ROUTE_COLOR}&${reset_color}/"\
     -e "s/^\([0-9]\+: \+\)\([^ \t]\+\)\(.* UP .*$\)/\1${INTERFACE_COLOR}\2${reset_color}\3/"\
     -e "s/^\([0-9]\+: \+\)\([^ \t]\+\)\(.* UNKNOWN .*$\)/\1${INTERFACE_COLOR}\2${reset_color}\3/"\
